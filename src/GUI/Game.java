72994260ecbe5f9945fac53d8e349181cc987f2f
s1100968 <s1100968@WS1100968.bc.local>
2013-03-20 13:33:49 +0200

    Sankarin liike kuvat

--- src/GUI/Game.java
+++ src/GUI/Game.java
@@ -9,9 +9,12 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Game extends BasicGame {
-
-    Image plane = null;
+    Image hero = null;
     Image land = null;
+    Image[] heroS = {null, null, null};
+    Image[] heroD = {null, null, null};
+    Image[] heroA = {null, null, null};
+    Image[] heroW = {null, null, null};
     float x = 400;
     float y = 300;
     float speed = 0.1f;
@@ -23,9 +26,33 @@ public class Game extends BasicGame {
     @Override
     public void init(GameContainer gc)
             throws SlickException {
-        plane = new Image("images/hero.png");
         land = new Image("images/land.jpg");
-
+       
+      
+        for (int i = 0; i < 3; i++) {
+            heroS[i] = new Image("images/heroS0" + i + ".png");
+            // TODO muut heroU = new Image("images") ...
+        }
+        hero = heroS[0];
+       
+        
+        for (int i = 0; i < 3; i++) {
+            heroD[i] = new Image("images/heroD0" + i + ".png");
+            // TODO muut heroU = new Image("images") ...
+        }
+        hero = heroD[0];
+        
+        for (int i = 0; i < 3; i++) {
+            heroA[i] = new Image("images/heroA0" + i + ".png");
+            // TODO muut heroU = new Image("images") ...
+        }
+        hero = heroA[0];
+        
+        for (int i = 0; i < 3; i++) {
+            heroW[i] = new Image("images/heroW0" + i + ".png");
+            // TODO muut heroU = new Image("images") ...
+        }
+        hero = heroW[0];
     }
 
     @Override
@@ -35,12 +62,32 @@ public class Game extends BasicGame {
 
         if (input.isKeyDown(Input.KEY_A)) {
             x -= speed * delta;
+            if (hero == heroA[1]) {
+                hero = heroA[2];
+            } else {
+                hero = heroA[1];
+            }
         } else if (input.isKeyDown(Input.KEY_D)) {
             x += speed * delta;
+            if (hero == heroD[1]) {
+                hero = heroD[2];
+            } else {
+                hero = heroD[1];
+            }
         } else if (input.isKeyDown(Input.KEY_W)) {
             y -= speed * delta;
+            if (hero == heroW[1]) {
+                hero = heroW[2];
+            } else {
+                hero = heroW[1];
+            }
         } else if (input.isKeyDown(Input.KEY_S)) {
             y += speed * delta;
+              if (hero == heroS[1]) {
+                hero = heroS[2];
+            } else {
+                hero = heroS[1];
+        }
         }
     }
 
@@ -48,7 +95,7 @@ public class Game extends BasicGame {
             throws SlickException {
         land.draw(0, 0);
 
-        plane.draw(x, y);
+        hero.draw(x, y);
 
     }
 
@@ -56,8 +103,9 @@ public class Game extends BasicGame {
             throws SlickException {
         AppGameContainer app =
                 new AppGameContainer(new Game());
-
         app.setDisplayMode(800, 600, false);
         app.start();
+
+      
     }
 }
